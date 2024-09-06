225658de5ae29dba2de0b75c493cde07c60f9f07
PetxCode <brighterdayscodelab@gmail.com>
2024-09-06 19:33:46 +0100

    wrd

--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -1,22 +1,43 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../layout/Layout";
-import Features from "../pages/Features";
-import SignUp from "../pages/(auth)/SignUp";
-import LandingPage from "../pages/LandingPage";
+import LayoutScreen from "../static/LayoutScreen";
+import UserLayout from "../static/userLayout";
+import AuthLayout from "../static/AuthLayloat";
+import Register from "../page/auth/Registration";
+import Login from "../page/auth/LoginScreen";
+import LandingScreen from "../page/screen/LandingScreen";
+import PrivateRouter from "./privateRouter";
+import HomeScreen from "../page/home/HomeScreen";
 
 export const mainRouter = createBrowserRouter([
   {
-    element: <Layout />,
+    path: "/",
+    element: <LayoutScreen />,
+    children: [{ path: "/", element: <LandingScreen /> }],
+  },
+  {
+    path: "/user",
+    element: (
+      <PrivateRouter>
+        {" "}
+        <UserLayout />
+      </PrivateRouter>
+    ),
+    children: [{ index: true, element: <HomeScreen /> }],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
     children: [
-      { index: true, element: <LandingPage /> },
       {
-        path: "/features",
-        element: <Features />,
+        index: true,
+        element: <Register />,
       },
       {
-        path: "/signup",
-        element: <SignUp />,
+        path: "login",
+        index: true,
+        element: <Login />,
       },
     ],
   },
+  { path: "*", element: <h1>404 Not Found</h1> },
 ]);
